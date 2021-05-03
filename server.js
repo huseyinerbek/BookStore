3d78048989167bdbbceefd1ae259cd691caf12d5
Hüseyin ERBEK <erbekhuseyin@gmail.com>
2021-05-03 23:34:45 +0300

    Intial Author Routes

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({ limit: '2mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true
+    useNewUrlParser:true,
+    useUnifiedTopology: true 
 })
 
 const db = mongoose.connection
@@ -26,5 +29,6 @@ db.once('open',() => console.log('Connected to Mongoose'))
 
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000)
