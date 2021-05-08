25846c6e331a69ff6a58a001074cee5ff774cd05
Hüseyin ERBEK <erbekhuseyin@gmail.com>
2021-05-08 21:58:33 +0300

    Finished BackEnd

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Book = require("../models/book");
 const Author = require("../models/author");
-const imageMimeTypes = ['image/jpeg','image/png','images/gif']
+const imageMimeTypes = ['image/jpeg','image/jpg','image/png','images/gif']
 
 // All Books Route
 router.get("/", async (req, res) => {
@@ -46,34 +46,116 @@ router.post("/", async (req, res) => {
 
   try {
     const newBook = await book.save()
-    //! res.redirect(`books/${newAuthor.id}`)
-    res.redirect("books")
+    res.redirect(`books/${newBook.id}`)
   } catch {
     renderNewPage(res, book, true)
   }
-});
+})
+
+// Show book Route
+router.get('/:id', async(req,res)=>{
+  try {
+    //? Burada Author  id alarak o Authorun bilgilerini getiriyoruz
+    const book = await Book.findById(req.params.id).populate('author').exec()
+    res.render('books/show', {book:book})
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Edit Book Route
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+    renderEditPage(res, book);
+  } catch  {
+    res.redirect('/')
+  }
+
+})
+
+// Update Book Route
+router.put('/:id', async (req, res) => {
+  let book
+  try {
+    book = await Book.findById(req.params.id)
+    book.title = req.body.title
+    book.author = req.body.author
+    book.publishDate = new Date(req.body.publishDate)
+    book.pageCount = req.body.pageCount
+    if(req.body.cover != null && req.body.cover !== ''){
+      console.log(req.body.cover)
+      saveCover(book, req.body.cover)
+    }
+    await book.save()
+    res.redirect(`/books/${book.id}`)
+  } catch (err){
+    console.log(err)
+    if(book != null){
+      renderEditPage(res, book, true)
+    }else{
+      res.redirect('/')
+    }
+  }
+})
+// Delete Book Page
+router.delete('/:id', async (req,res)=>{
+  let book
+  try {
+    book = await Book.findById(req.params.id)
+    await book.remove()
+    res.redirect('/books')
+  } catch {
+    if(book != null){
+      res.render('book/show',{
+        book: book,
+        errorMessage: 'Could not remove Book'
+      })
+    } else{
+      res.redirect('/')
+    }
+  }
+})
 
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book ,'new', hasError )
+}
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book ,'edit', hasError )
+}
+
+
+async function renderFormPage(res, book, form, hasError = false) {
   try {
     const authors = await Author.find({});
     const params = {
       authors: authors,
       book: book
-    };
-    if (hasError) params.errorMessage = 'Error Creating Book'
-    res.render('books/new', params);
-  } catch {
+    }
+    if(hasError){
+      if(form === 'edit'){
+        params.errorMessage = 'Error Updating Book'
+      }else{
+        params.errorMessage = 'Error Creating Book'
+      }
+    }
+    res.render(`books/${form}`, params);
+  } catch{
+
     res.redirect('/books')
   }
 }
 
 function saveCover(book, coverEncoded) {
-  if (coverEncoded == null) return
-  const cover = JSON.parse(coverEncoded)
+
+  if (coverEncoded == null && cover == '') return
+  const cover = JSON.parse(JSON.stringify(coverEncoded))
   if (cover != null && imageMimeTypes.includes(cover.type)) {
     book.coverImage = new Buffer.from(cover.data, 'base64')
-    book.coverImageType = cover.type;
+    book.coverImageType = cover.type
   }
 }
+
 
 module.exports = router
